94f69740fd85ef37a2430e32b76e6b5e40c6c7b1
Miha Benčina <mb6315@student.uni-lj.si>
2022-02-25 16:03:45 +0200

    static arrow shoot

--- Assets/Scripts/ArrowShoot.cs
+++ Assets/Scripts/ArrowShoot.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class ArrowShoot : MonoBehaviour
 {
+    public GameObject arrow;
+    Vector3 direction = new Vector3(-1, 0, 0);
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Start");
+        //arrow = GameObject.Find("Arrow").GetComponent<GameObject>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // arrow.transform() +=  * 10.0f;
+        // transform.position = Vector3.forward;
+        transform.Translate(direction * 1.5f * Time.deltaTime);
     }
 }
