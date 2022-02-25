2d382c0a0b6671d0f7331761f14e27ee953ae9cc
Janne Vikelä <->
2022-02-25 16:53:06 +0200

    Crosshair implementation first version
    Crosshair returns the object and the component it points to using raycast.

---
+++ Assets/raycastTest.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class raycastTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // void Start()
+    // {
+        
+    // }
+
+    public Transform Crosshair;
+    public Transform Gun;
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit Hit;
+
+        if (Physics.Raycast(Gun.transform.position, Gun.transform.forward, out Hit))
+        {
+
+                Crosshair.transform.localPosition = Hit.transform.position;
+                Debug.Log(Hit.transform.position);
+            
+        }
+
+    }
+}
