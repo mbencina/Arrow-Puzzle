ff135352016afd40c63285d581ecdb5d66365d74
showrov016 <showrov016@gmail.com>
2022-03-04 15:07:02 +0200

    testing puzzle positions

---
+++ Assets/Scenes/puzzlePlacement3.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class puzzlePlacement3 : MonoBehaviour
+{
+    public GameObject Puzzle1;
+    public GameObject Pos1;
+    public float Distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Distance = Vector3.Distance(Puzzle1.transform.position, Pos1.transform.position);
+        if(Distance>2){
+            Puzzle1.transform.position = Pos1.transform.position;
+        }
+
+    }
+
+}
