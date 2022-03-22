4ec39651d1165ba6d79919cbace69882527e13de
showrov016 <showrov016@gmail.com>
2022-03-22 23:50:42 +0200

    puzzle placement feature added for test

--- Assets/Scripts/PuzzlePlacement.cs
+++ Assets/Scripts/PuzzlePlacement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class puzzlePlacement1 : MonoBehaviour
+public class PuzzlePlacement : MonoBehaviour
 {
     GameObject puzzle;
     public GameObject pos;
