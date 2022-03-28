dd40230b9c49def7f8ca08942395790fe9b9b727
helipoikela <92163340+helipoikela@users.noreply.github.com>
2022-03-28 12:46:38 +0300

    Adding audio to HUD

--- Assets/Tests/Scripts/PanelOpener.cs
+++ Assets/Tests/Scripts/PanelOpener.cs
@@ -4,42 +4,79 @@ using UnityEngine;
 
 namespace Menus { 
 
-public class PanelOpener : MonoBehaviour
-{
-    /// <summary>
-    /// Panel for displaying congratulation messages
-    /// </summary>
-    public GameObject Panel;
+    public class PanelOpener : MonoBehaviour
+    {
+        /// <summary>
+        /// Panel for displaying congratulation messages
+        /// </summary>
+        public GameObject Panel;
+        /// <summary>
+        /// Truth value for when to open the panel
+        /// </summary>
+        public bool open = false;
+        /// <summary>
+        /// The audio that plays when the puzzle is completed
+        /// </summary>
+        public new AudioSource audio;
 
-    private void Start()
-    {
-        StartCoroutine(ShowMessage());
-    }
-
-    public IEnumerator ShowMessage()
-    {
-        for (int i = 0; i < 10; i++)
+        private void Start()
         {
-            yield return new WaitForSecondsRealtime(3);
-            OpenPanel();
-            
-            yield return new WaitForSecondsRealtime(2);
-            OpenPanel();
-        }
-    }
-    
-    /// <summary>
-    /// Opens the panel where the congratulation message is displayed
-    /// </summary>
-    public void OpenPanel()
-    {
-        if (Panel != null)
-        {
-            bool isActive = Panel.activeSelf;
-            Debug.Log("Panel " + isActive);
-            Panel.SetActive(!isActive);
+            StartCoroutine(ShowMessage());
         }
 
+        /*private void Update()
+        {
+            if (open)
+            {
+                OpenPanel();
+            }
+        }*/
+
+        public IEnumerator ShowMessage()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                /* yield return new WaitForSecondsRealtime(3);
+                 OpenPanel();
+
+                 yield return new WaitForSecondsRealtime(2);
+                 OpenPanel();*/
+
+                yield return new WaitForSecondsRealtime(2);
+
+                if (i == 2 || i == 4 || i == 6 || i == 8)
+                {
+                    open = true;
+                }
+                else
+                {
+                    open = false;
+                }
+
+                if (open)
+                {
+                    OpenPanel();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Opens the panel where the congratulation message is displayed
+        /// </summary>
+        public void OpenPanel()
+        {
+            if (Panel != null)
+            {
+                bool isActive = Panel.activeSelf;
+                Debug.Log("Panel " + isActive);
+                Panel.SetActive(!isActive);
+                if (open == true)
+                {
+                    audio.Play(0);
+                }
+               
+            }
+
+        }
     }
-}
 }
