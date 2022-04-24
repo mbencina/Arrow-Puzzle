0dfe2b8f1eca4b1851a1b274898edeb7a5b29cf9
Miha Benčina <mb6315@student.uni-lj.si>
2022-04-24 17:31:22 +0300

    add comments

--- Assets/Scripts/NewShooting.cs
+++ Assets/Scripts/NewShooting.cs
@@ -13,7 +13,7 @@ namespace Shooting
         // reference to the gun i.e. bow:
         public Transform Gun;
 
-        // reference to right hand controller
+        // reference to right hand controller (actually left hand in the end)
         public Transform RightHand;
 
         // adjustable maximal distance before arrow spawns again in hand
@@ -34,16 +34,22 @@ namespace Shooting
         // adjustable arrow speed
         public float arrowSpeed = 10.0f;
 
+        // direction in which the arrow is shot
         private Vector3 direction = new Vector3(0, 0, 1);
-        private Vector3 initialPosition = new Vector3(0, 0, 0);
+
+        // whether the arrow is in the air flying or not
         private bool arrowFlying = false;
+
+        // is true if the arrow will hit a puzzle
         private bool puzzleHit = false;
+
+        // RaycastHit object
         private RaycastHit Hit;
 
         // Audio sources
         public AudioSource hitSound;
         public AudioSource releaseSound;
-
+ 
         private void Awake()
         {
             inputAction.action.started += DoAction;
@@ -59,21 +65,24 @@ namespace Shooting
         
             if (!arrowFlying && shootAnother) {
             
-                direction = Gun.transform.forward; // -Gun.transform.right
+                // setting arrow direction to raycast of bow at the moment of shooting
+                direction = Gun.transform.forward;
+                // correctly rotate the arrow
                 arrow.transform.rotation = Quaternion.LookRotation(direction);
-                // Debug.Log("direction: " + Gun.transform.forward);
-                // Debug.Log(-Gun.transform.right);
+
+                // technicalities to make the arrow shoot look better
                 gameObject.transform.position = RightHand.transform.position;
                 fakeArrow.SetActive(false);
                 arrow.SetActive(true);
-                // TODO adjust real arrow location to fake arrows location - test!
 
-                if (Physics.Raycast(Gun.transform.position, Gun.transform.forward, out Hit, 50) && Hit.transform.name.Contains("Plane")) //  && Hit.transform.name.Contains("Plane")
+                // this way we find out early if we'll hit a puzzle since arrow ignores gravity
+                if (Physics.Raycast(Gun.transform.position, Gun.transform.forward, out Hit, 50) && Hit.transform.name.Contains("Plane"))
                 {
                     puzzleHit = true;
                     outScript.pieceHit = Hit;
                 }
-                // Sound for when the array is released 
+                
+                // Sound for when the arrow is released 
                 releaseSound.Play(0);
                 arrowFlying = true;
                 shootAnother = false;
