3edf3cce6d5f7843e1d678f43c94a9e437346b68
helipoikela <92163340+helipoikela@users.noreply.github.com>
2022-04-26 10:32:46 +0300

    Adding instructions to the main menu

--- Assets/Scripts/SwitchMenuCanvas.cs
+++ Assets/Scripts/SwitchMenuCanvas.cs
@@ -19,9 +19,13 @@ namespace Menus
         /// </summary>
         public GameObject Credits;
         /// <summary>
-        /// Tutorial canvas
+        /// Tutorial canvas in english
         /// </summary>
-        public GameObject Tutorial;
+        public GameObject TutorialEn;
+        /// <summary>
+        /// Tutorial canvas in finnish
+        /// </summary>
+        public GameObject TutorialFi;
 
         /// <summary>
         /// Switches the canvas that is shown in the menu
@@ -39,21 +43,32 @@ namespace Menus
             {
                 Menu.SetActive(true);
                 Credits.SetActive(false);
-                Tutorial.SetActive(false);
+                TutorialEn.SetActive(false);
+                TutorialFi.SetActive(false);
             }
             // If credits should be shown.
             else if (name == "Credits")
             {
                 Menu.SetActive(false);
                 Credits.SetActive(true);
-                Tutorial.SetActive(false);
+                TutorialEn.SetActive(false);
+                TutorialFi.SetActive(false);
             }
-            // If tutorial should be shown.
-            else if (name == "Tutorial")
+            // If english tutorial should be shown.
+            else if (name == "TutorialEn")
             {
                 Menu.SetActive(false);
                 Credits.SetActive(false);
-                Tutorial.SetActive(true);
+                TutorialEn.SetActive(true);
+                TutorialFi.SetActive(false);
+            }
+            // If tutorial should be shown.
+            else if (name == "TutorialFi")
+            {
+                Menu.SetActive(false);
+                Credits.SetActive(false);
+                TutorialEn.SetActive(false);
+                TutorialFi.SetActive(true);
             }
         }
     }
